55594d7a501875a8f2e94b2aca814116c72fca68
arthur d'helft <adhelft1@myges.fr>
2024-11-20 12:57:08 +0100

    feat: ajout de la multiplicationv3

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """module pour la multiplication  """
 def multiplication(valeur, multi):
-     """cette methon fait un multiplication"""
-     return valeur*multi
+    """cette methon fait un multiplication"""
+    return valeur*multi
 
 print(multiplication(2,5))
